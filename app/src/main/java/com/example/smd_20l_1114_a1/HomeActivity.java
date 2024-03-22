aab20875273404359bf093e72ed9c66f07d0261d
komalw2001 <komalwaseemm@gmail.com>
2024-03-22 23:20:13 +0500

    added fragments

--- app/src/main/java/com/example/smd_20l_1114_a1/HomeActivity.java
+++ app/src/main/java/com/example/smd_20l_1114_a1/HomeActivity.java
@@ -2,13 +2,37 @@ package com.example.smd_20l_1114_a1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ImageView ivArrow;
+    private Button btnHome, btnProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        init();
+
+        ivArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void init() {
+        ivArrow = findViewById(R.id.ivArrow);
+        btnHome = findViewById(R.id.btnHome);
+        btnProfile = findViewById(R.id.btnProfile);
     }
 }
